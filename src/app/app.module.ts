ddb1109637579f0fd87b92b0d5cb4a666b52f016
Naman Arora <namanarora146@gmail.com>
2020-09-29 20:07:02 +0530

    It's First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoHeaderComponent } from './todo/todo-header/todo-header.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoHeaderComponent,
+    TodoFooterComponent
   ],
   imports: [
     BrowserModule,
