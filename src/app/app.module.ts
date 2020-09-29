03e6d1386321d41397272e59b89b4dd956eaaf26
Naman Arora <namanarora146@gmail.com>
2020-09-29 21:25:30 +0530

    @nd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoHeaderComponent } from './todo/todo-header/todo-header.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoInputComponent } from './todo/todo-input/todo-input.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoHeaderComponent,
-    TodoFooterComponent
+    TodoFooterComponent,
+    TodoInputComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
